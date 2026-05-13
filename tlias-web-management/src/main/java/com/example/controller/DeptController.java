4b3f8e317826742cf3fb18e7db9a9a87031af144
Your Name <you@example.com>
2026-05-13 16:19:18 +0800

    init

---
+++ tlias-web-management/src/main/java/com/example/controller/DeptController.java
@@ -0,0 +1,59 @@
+package com.example.controller;
+
+import com.example.pojo.Dept;
+import com.example.pojo.Result;
+import com.example.service.DeptService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @RequestMapping("/depts")
+    public Result list() {
+//        System.out.println("查询全部部门数据");
+        log.info("查询全部部门数据");
+        List<Dept> depList = deptService.findAll();
+        return Result.success(depList);
+    }
+
+    @DeleteMapping("/depts")
+    public Result delete(Integer id) {
+//        System.out.println("删除部门数据，id：" + id);
+        log.info("删除部门数据，id：" + id);
+        deptService.deleteById(id);
+        return Result.success();
+    }
+
+    @PostMapping("/depts")
+    public Result add(@RequestBody Dept dept) {
+//        System.out.println("添加部门数据：" + dept);
+        log.info("添加部门数据：" + dept);
+        deptService.add(dept);
+        return Result.success();
+    }
+
+    @GetMapping("/depts/{id}")
+    public Result get(@PathVariable Integer id) {
+//        System.out.println("查询部门数据，id：" + id);
+        log.info("查询部门数据，id：" + id);
+        Dept dept = deptService.getById(id);
+        return Result.success(dept);
+    }
+
+    @PutMapping("/depts")
+    public Result update(@RequestBody Dept dept) {
+//        System.out.println("修改部门数据：" + dept);
+        log.info("修改部门数据：" + dept);
+        deptService.update(dept);
+        return Result.success();
+    }
+}
---
+++ tlias-web-management/src/main/java/com/example/service/impLDeptServiceimpl.java
@@ -0,0 +1,46 @@
+package com.example.service;
+
+import com.example.mapper.DeptMapper;
+import com.example.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class impLDeptServiceimpl implements DeptService{
+    @Autowired
+    private DeptMapper deptMapper;
+
+    @Override
+    public List<Dept> findAll() {
+        return deptMapper.findAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        deptMapper.deleteById(id);
+    }
+
+    @Override
+    public void add(Dept dept) {
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+
+        deptMapper.insert(dept);
+    }
+
+    @Override
+    public Dept getById(Integer id) {
+        Dept dept = deptMapper.getById(id);
+        return dept;
+    }
+
+    @Override
+    public void update(Dept dept) {
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.update(dept);
+    }
+}
